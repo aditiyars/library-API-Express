87d4c34ae714ecb861bc0582aab48ae2f2989361
Aditiya <aditiya.saputra@bina-artha.co.id>
2024-06-02 13:38:53 +0700

    add documentation

--- src/swagger.js
+++ src/swagger.js
@@ -1,6 +1,6 @@
 const swaggerJsdoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
-const docs = require('./documentation/swagger.json')
+const docs = require('./documentation/openapi.json')
 
 module.exports = (app) => {
     app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(docs))
